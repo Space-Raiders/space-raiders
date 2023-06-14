047a091f3e3a9bb5741e24ed91c1c1b8f6f94c06
Mohammed-MW <mohammed.maouedj.pro@gmail.com>
2023-06-14 14:35:29 +0200

    test

--- src/app/api/api.component.spec.ts
+++ src/app/api/api.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+
 import { ApiComponent } from './api.component';
+
 
 describe('ApiComponent', () => {
   let component: ApiComponent;
   let fixture: ComponentFixture<ApiComponent>;
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,6 +17,7 @@ describe('ApiComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -1,5 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { ConfigService } from '../config.service';
+// import { ConfigService } from '../config.service';
+
+
+interface Body {
+  englishName: string;
+  // Add other properties as needed
+}
+
+
+interface Response {
+  bodies: Body[];
+}
+
+
 
 @Component({
   selector: 'app-api',
@@ -7,15 +22,67 @@ import { ConfigService } from '../config.service';
   styleUrls: ['./api.component.scss']
 })
 export class ApiComponent {
-  planetName = "api";
+  planetDetails: any;
+  planetName = '';
 
-  constructor(private api: ConfigService) {
 
+  constructor(private solarSystemService: ConfigService) { }
+
+
+  searchPlanet(): void {
+    this.solarSystemService.getPlanetDetails(this.planetName).subscribe(
+      (data) => {
+        this.planetDetails = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-  ngOnInit() {
-    this.api.apiCall().subscribe((data: any) => {
-      console.warn("get api data", data);
-      this.planetName = data['id = 2295434']
-    })
-  }
+
+
+  /////////////////////////////////
 }
+
+
+
+
+// import { Component } from '@angular/core';
+// import { ConfigService } from '../config.service';
+// import { ApiContent } from '../api-content';
+
+
+// @Component({
+//   selector: 'app-api',
+//   templateUrl: './api.component.html',
+//   styleUrls: ['./api.component.scss']
+// })
+// export class ApiComponent {
+//   planetName = "api";
+
+
+//   constructor(private api: ConfigService) {
+
+
+//   }
+
+
+//   lstApiContent: ApiContent[] | undefined;
+
+
+//   ngOnInit() {
+//     this.api.apiCall().subscribe((data: any) => {
+//       this.lstApiContent = data;
+//     })
+//   }
+// }
+
+
+// // ngOnInit() {
+// //   this.api.apiCall().subscribe((data: any) => {
+// //     this.lstApiContent = data;
+// //   })
+// // }
+
+
+
